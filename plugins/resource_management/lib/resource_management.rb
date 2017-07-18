cbc3867dc267846c1b85a642b04510ba9ba8045e
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-07-18 13:48:53 +0000

    remove old service layer

--- plugins/resource_management/lib/resource_management.rb
+++ plugins/resource_management/lib/resource_management.rb
@@ -1,7 +1,7 @@
 require "resource_management/engine"
 # use misty with our limes-resources extension
 require 'misty/openstack/limes'
-
+# load resource management related config
 require_relative "resource_management/package_config"
 require_relative "resource_management/resource_config"
 require_relative "resource_management/service_config"
